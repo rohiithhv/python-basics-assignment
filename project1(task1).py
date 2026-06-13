355ecf76963d9b1fd5d74f640264f54abb742ca3
Rohith v <rohithuzi28@gmail.com>
2026-06-14 00:35:21 +0530

    Add files via upload

---
+++ project1(task1).py
@@ -0,0 +1,10 @@
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+print("addition:",add)
+print("subtraction:",sub)
+print("multiplication:",mul)
+print("division:",div)
---
+++ project1(task2).py
@@ -0,0 +1,4 @@
+firstname= input("enter first name:")
+lastname=input("enter last name:")
+name=firstname+lastname
+print("hello,",name ,"welcome to the program")
